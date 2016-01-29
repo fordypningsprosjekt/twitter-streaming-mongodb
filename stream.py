d6b8c6671831ead1454cf7e572c741b030ba76f8
Asbjørn Steinskog <asbjorn@steinskog.me>
2016-01-29 14:07:35 +0100

    Catch exception

--- stream.py
+++ stream.py
@@ -14,25 +14,27 @@ tweets = []
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
-        if len(tweets)>100000:
-            return False
+        try:
+            if len(tweets)>100000:
+                return False
 
-        tweet = json.loads(data)
+            tweet = json.loads(data)
 
-
-        if "created_at" in tweet:
-            if tweet["lang"]=="en": 
-                    if not tweet["entities"]["urls"]:
-                        if not "retweeted_status" in tweet:
-                            if not "media" in tweet["entities"]:
-                                    if tweet["text"] > 5:
-                                        if not "#MTVStars" in tweet["text"]:
-                                            if not "Weather Channel" in tweet["text"]:
-                                                tweets.append(json.loads(data))
-                                                file_.write(data)
-                                                if len(tweets)%50==0:
-                                                    print tweet["text"]
-        return True
+            if "created_at" in tweet:
+                if tweet["lang"]=="en": 
+                        if not tweet["entities"]["urls"]:
+                            if not "retweeted_status" in tweet:
+                                if not "media" in tweet["entities"]:
+                                        if tweet["text"] > 5:
+                                            if not "#MTVStars" in tweet["text"]:
+                                                if not "Weather Channel" in tweet["text"]:
+                                                    tweets.append(json.loads(data))
+                                                    file_.write(data)
+                                                    if len(tweets)%50==0:
+                                                        print tweet["text"]
+            return True
+        except:
+            print "fail"
 
     def on_error(self, status):
         print status
@@ -44,6 +46,5 @@ if __name__ == '__main__':
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-
     stream = Stream(auth, l)
     stream.sample()
